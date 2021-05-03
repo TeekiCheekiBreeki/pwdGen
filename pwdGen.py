e1e13a25f6d0fd779b7d443ba32f095fc2fd718b
Tian Grumerec <tgrumerec@gmail.com>
2021-05-03 13:29:01 +0200

    Password is saved to .txt file with name of account

--- pwdGen.py
+++ pwdGen.py
@@ -2,9 +2,9 @@ import random
 import string
 #implementing this later
 
-#print("For which account you want your password: ")
+print("For which account you want your password: ")
 
-#account = input();
+account = input();
 
 
 length = int(input('\nHow long do you want your password to be: '))
@@ -18,5 +18,11 @@ all_symbols = lower + upper + num + symbols
 #we generate <length> long password and print it out
 temp = random.sample(all_symbols,length)
 password = "".join(temp)
+
+
+# We save password to the passwords.txt
+text_file = open(r'C:\Users\teeki\Documents\Passwords\passwords.txt' , 'a')
+n = text_file.write('\n' + account + ' password: ' + password)
+text_file.close()
 
 print("Your password is: ", password)
